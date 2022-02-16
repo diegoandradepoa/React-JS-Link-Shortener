541df91a92d2fc25c2306fb9b96fa20823ab064e
Diego de Andrade Cruz <diegoandradecruz15@gmail.com>
2022-02-16 09:48:15 -0300

    adjustment .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-export const key = '054638913ac4f38d55ace6700e78e17acb173885';
-
 const api = axios.create({
     baseURL: 'https://api-ssl.bitly.com/v4',
     headers:{
-        'Authorization': `Bearer ${key}`
+        'Authorization': `Bearer ${process.env.KEY}`
     }
 })
 
 export default api;
 
-// minha chave: 054638913ac4f38d55ace6700e78e17acb173885
