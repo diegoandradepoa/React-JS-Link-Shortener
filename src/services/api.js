66d871709bfce971db5e78070111b9bc74ea74cf
Diego de Andrade Cruz <diegoandradecruz15@gmail.com>
2022-02-16 10:03:04 -0300

    key adjustments

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+import authorization from './key';
+
 const api = axios.create({
     baseURL: 'https://api-ssl.bitly.com/v4',
     headers:{
-        'Authorization': `Bearer ${process.env.KEY}`
+        'Authorization': `Bearer ${authorization()}`
     }
 })
 
---
+++ src/services/key.js
@@ -0,0 +1,6 @@
+
+export default function authorization(){
+    const key = '054638913ac4f38d55ace6700e78e17acb173885'
+    return key
+}
+
